4d20a58f9229279e8f0c7f9f27cfd5ddff815e8f
Ivan Šakić <ivan.sakic@flownform.com>
2022-03-08 13:57:35 +0100

    Refactor and update plugin

--- src/index.ts
+++ src/index.ts
@@ -1,178 +1,182 @@
-import { Platform, Dimensions } from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
-import Constants from "expo-constants";
-import { Buffer } from "buffer";
+import { Platform, Dimensions } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import Constants from 'expo-constants'
+import Device from 'expo-device'
+import { Buffer } from 'buffer'
 
-const MIXPANEL_API_URL = "https://api.mixpanel.com";
+const DEFAULT_MIXPANEL_API_URL = 'https://api.mixpanel.com'
+const DEFAULT_STORAGE_KEY = 'mixpanel:super:props'
+
+type Props = Record<string, unknown>
+
+export interface ExpoMixpanelAnalyticsConfig {
+  clientId?: string
+  storageKey?: string
+  apiUrl?: string
+}
+
+export interface Event {
+  name: string
+  sent: boolean
+  props?: Props
+}
 
 export class ExpoMixpanelAnalytics {
-  ready = false;
-  token: string;
-  storageKey: string;
-  userId?: string | null;
-  clientId?: string;
-  platform?: string;
-  model?: string;
-  queue: any[] = [];
-  constants: { [key: string]: string | number | void } = {};
-  superProps: any = {};
-
-  constructor(token, storageKey = "mixpanel:super:props") {
-    this.storageKey = storageKey;
-
-    this.token = token;
-    this.userId = null;
-    this.clientId = Constants.deviceId || Constants.installationId;
-    this.constants = {
-      app_build_number: Constants.manifest?.revisionId,
-      app_id: Constants.manifest?.slug,
-      app_name: Constants.manifest?.name,
-      app_version_string: Constants.manifest?.version,
-      device_name: Constants.deviceName,
-      expo_app_ownership: Constants.appOwnership || undefined,
-      os_version: Platform.Version,
-    };
-
-    Constants.getWebViewUserAgentAsync().then((userAgent) => {
-      // @ts-ignore
-      const { width, height } = Dimensions.get("window");
-      Object.assign(this.constants, {
-        screen_height: height,
-        screen_size: `${width}x${height}`,
-        screen_width: width,
-        user_agent: userAgent,
-      });
-      if (
-        Platform.OS === "ios" &&
-        Constants.platform &&
-        Constants.platform.ios
-      ) {
-        this.platform = Constants.platform.ios.platform;
-        this.model = Constants.platform.ios.model;
-      } else {
-        this.platform = "android";
-      }
-
-      AsyncStorage.getItem(this.storageKey, (_, result) => {
-        if (result) {
-          try {
-            this.superProps = JSON.parse(result) || {};
-          } catch {}
-        }
-
-        this.ready = true;
-        this._flush();
-      });
-    });
+  ready = false
+  storageKey: string
+  apiUrl: string
+  userId?: string
+  clientId?: string
+  platform?: string
+  model?: string
+  queue: Event[] = []
+  superProps: Props = {}
+  constants: Props = {
+    app_build_number: Constants.manifest?.revisionId,
+    app_id: Constants.manifest?.slug,
+    app_name: Constants.manifest?.name,
+    app_version_string: Constants.manifest?.version,
+    device_name: Constants.deviceName,
+    expo_app_ownership: Constants.appOwnership || undefined,
+    os_version: Platform.Version,
   }
 
-  register(props: any) {
-    this.superProps = props;
+  constructor (
+    public token: string,
+    config?: ExpoMixpanelAnalyticsConfig,
+  ) {
+    this.storageKey = config?.storageKey ?? DEFAULT_STORAGE_KEY
+    this.apiUrl = config?.apiUrl ?? DEFAULT_MIXPANEL_API_URL
+    this.clientId = config?.clientId
+  }
+
+  async init () {
+    const userAgent = await Constants.getWebViewUserAgentAsync()
+
+    const { width, height } = Dimensions.get('window')
+
+    Object.assign(this.constants, {
+      screen_height: height,
+      screen_size: `${width}x${height}`,
+      screen_width: width,
+      user_agent: userAgent,
+    })
+
+    if (Platform.OS === 'ios' && Constants.platform?.ios) {
+      this.platform = Device.modelId ?? undefined
+      this.model = Device.modelName ?? undefined
+    } else {
+      this.platform = 'android'
+    }
+
     try {
-      AsyncStorage.setItem(this.storageKey, JSON.stringify(props));
+      const result = await AsyncStorage.getItem(this.storageKey)
+      this.superProps = JSON.parse(result ?? '{}')
+      this.ready = true
+      this._flush()
     } catch {}
   }
 
-  track(name: string, props?: any) {
+  async register (props: Props) {
+    this.superProps = props
+    try {
+      await AsyncStorage.setItem(this.storageKey, JSON.stringify(props))
+    } catch {}
+  }
+
+  track (name: string, props?: Props) {
     this.queue.push({
       name,
       props,
-    });
-    this._flush();
+      sent: false
+    })
+    this._flush()
   }
 
-  identify(userId?: string) {
-    this.userId = userId;
+  identify (userId?: string) {
+    this.userId = userId
   }
 
-  reset() {
-    this.identify(this.clientId);
+  async reset () {
+    this.identify(this.clientId)
     try {
-      AsyncStorage.setItem(this.storageKey, JSON.stringify({}));
+      await AsyncStorage.setItem(this.storageKey, JSON.stringify({}))
     } catch {}
   }
 
-  people_set(props) {
-    this._people("set", props);
+  people_set (props: unknown) {
+    this._people('set', props)
   }
 
-  people_set_once(props) {
-    this._people("set_once", props);
+  people_set_once (props: unknown) {
+    this._people('set_once', props)
   }
 
-  people_unset(props) {
-    this._people("unset", props);
+  people_unset (props: unknown) {
+    this._people('unset', props)
   }
 
-  people_increment(props) {
-    this._people("add", props);
+  people_increment (props: unknown) {
+    this._people('add', props)
   }
 
-  people_append(props) {
-    this._people("append", props);
+  people_append (props: unknown) {
+    this._people('append', props)
   }
 
-  people_union(props) {
-    this._people("union", props);
+  people_union (props: unknown) {
+    this._people('union', props)
   }
 
-  people_delete_user() {
-    this._people("delete", "");
+  people_delete_user () {
+    this._people('delete', '')
   }
 
   // ===========================================================================================
 
-  _flush() {
+  private _flush () {
     if (this.ready) {
       while (this.queue.length) {
-        const event = this.queue.pop();
-        this._pushEvent(event).then(() => (event.sent = true));
+        const event = this.queue.pop()!
+        void this._pushEvent(event).then(() => event.sent = true)
       }
     }
   }
 
-  _people(operation, props) {
+  private _people (operation: string, props: unknown) {
     if (this.userId) {
       const data = {
         $token: this.token,
         $distinct_id: this.userId,
-      };
-      data[`$${operation}`] = props;
+        [`$${operation}`]: props
+      }
 
-      this._pushProfile(data);
+      void this._push(data)
     }
   }
 
-  _pushEvent(event) {
-    let data = {
+  private async _pushEvent (event: Event) {
+    const data = {
       event: event.name,
       properties: {
         ...this.constants,
-        ...(event.props || {}),
+        ...event.props,
         ...this.superProps,
-      },
-    };
-    if (this.userId) {
-      data.properties.distinct_id = this.userId;
-    }
-    data.properties.token = this.token;
-    data.properties.client_id = this.clientId;
-    if (this.platform) {
-      data.properties.platform = this.platform;
-    }
-    if (this.model) {
-      data.properties.model = this.model;
+        distinct_id: this.userId,
+        token: this.token,
+        client_id: this.clientId,
+        platform: this.platform,
+        model: this.model,
+      }
     }
 
-    const buffer = new Buffer(JSON.stringify(data)).toString("base64");
-
-    return fetch(`${MIXPANEL_API_URL}/track/?data=${buffer}`);
+    return this._push(data)
   }
 
-  _pushProfile(data) {
-    data = new Buffer(JSON.stringify(data)).toString("base64");
-    return fetch(`${MIXPANEL_API_URL}/engage/?data=${data}`);
+  private async _push (data: unknown) {
+    const base64Data = new Buffer(JSON.stringify(data)).toString('base64')
+    return fetch(`${this.apiUrl}/engage/?data=${base64Data}`)
   }
 }
 
-export default ExpoMixpanelAnalytics;
+export default ExpoMixpanelAnalytics
